8a80f0a42b5dccc6730159d3ca2686326c8b1aa5
Vinoth Sivaraj <vsivaraj@cricut.com>
2018-09-04 17:43:34 +0530

    first commit

---
+++ app/src/main/java/com/ps/rxrelay/myapplication/MainActivity.java
@@ -0,0 +1,68 @@
+package com.ps.rxrelay.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.jakewharton.rxrelay2.BehaviorRelay;
+import com.jakewharton.rxrelay2.PublishRelay;
+import com.jakewharton.rxrelay2.Relay;
+import com.jakewharton.rxrelay2.ReplayRelay;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*Relay<String> relay = BehaviorRelay.create();*/
+    /*Emits 3, subscribe 4, 5, 6*/
+
+    /*Relay<String> relay = PublishRelay.create();*/
+    /*Emits subscribe 4, 5, 6*/
+
+    /*Relay<String> relay = ReplayRelay.create();*/
+    /*Emits 1, 2, 3 subscribe 4, 5, 6*/
+
+    Relay<String> relay = ReplayRelay.create();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMeFunction(View view) {
+        relay.accept("one");
+        relay.accept("two");
+        relay.accept("three");
+        relay.subscribe(getObservable());
+        relay.accept("four");
+        relay.accept("five");
+        relay.accept("six");
+    }
+
+    Observer<String> getObservable() {
+        return new Observer<String>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(String value) {
+                System.out.println(value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+    }
+}
